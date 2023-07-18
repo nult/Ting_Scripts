88b46ce397f0b23e68892e126c19a0f7012f735a
Ting Lei <ting@July.local>
2023-07-17 20:28:50 -0400

    update fconv

--- MATLAB/fconv.m
+++ MATLAB/fconv.m
@@ -4,5 +4,5 @@ n=length(inputa)+length(inputb)-1;
 nwith0=2^(int32(log2(n))+1);
 A=fft(inputa,nwith0);
 B=fft(inputb,nwith0);
-temp_output=ifft(A*B);
-output=temp_output(1:n);
+temp_output=ifft(A.*B);
+output=real(temp_output(1:length(inputa)));
